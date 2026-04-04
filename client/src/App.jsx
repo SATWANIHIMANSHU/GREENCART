0385892c51ea6bbc7cd794bfc0297a9eacb453a1
Practice <satwanihimanshu35@gmail.com>
2026-04-05 00:39:01 +0530

    updated app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ import AddProduct from "./pages/seller/AddProduct";
 import ProductList from "./pages/seller/ProductList";
 import Orders from "./pages/seller/Orders";
 import Loading from "./components/Loading";
+import Verify from "./pages/Verify";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
